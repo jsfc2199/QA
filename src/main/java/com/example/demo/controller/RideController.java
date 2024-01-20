4d4e879cff4f8424f9777b4da84f6d9781b5b617
SOFKA\juan.franco <juan.franco@sofka.com.co>
2024-01-19 22:03:17 -0500

    Permit acceso por empleado de tipo Operador

--- src/main/java/com/example/demo/controller/RideController.java
+++ src/main/java/com/example/demo/controller/RideController.java
@@ -5,10 +5,7 @@ import com.example.demo.entity.Station;
 import com.example.demo.service.RideService;
 import com.example.demo.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/")
@@ -19,6 +16,11 @@ public class RideController {
     @PostMapping("create/ride")
     public Ride createEmployee(@RequestBody Ride ride) {
         return service.createRide(ride);
+    }
+
+    @GetMapping("get/allowEntry")
+    public boolean allowEntryToRide(@RequestParam Long ticketId, @RequestParam Long rideId) {
+        return service.allowEntry(ticketId, rideId);
     }
 
 }
--- src/main/java/com/example/demo/service/RideService.java
+++ src/main/java/com/example/demo/service/RideService.java
@@ -1,13 +1,14 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Employee;
-import com.example.demo.entity.Ride;
-import com.example.demo.entity.Station;
+import com.example.demo.entity.*;
 import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.repository.RideRepository;
 import com.example.demo.repository.StationRepository;
+import com.example.demo.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class RideService {
@@ -17,12 +18,29 @@ public class RideService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    public Ride createRide(Ride ride){
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
+    public Ride createRide(Ride ride) {
         Employee employee = employeeRepository.findById(ride.getEmployeeId()).get();
+
+        if (!employee.getRole().equals("Operador")) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Empleado no es de tipo operador");
+        }
+
         employee.setRide(ride);
 
         rideRepository.save(ride);
         employeeRepository.save(employee);
         return ride;
     }
+
+    public boolean allowEntry(Long ticketId, Long rideId) {
+        Usuario usuario = usuariosRepository.findById(ticketId).get();
+        Ride ride = rideRepository.findById(rideId).get();
+
+        return ride.getMinHeight() < usuario.getHeight();
+    }
+
+
 }
